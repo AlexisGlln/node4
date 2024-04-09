108b4d44a97584d8dbe7a1c36922b0390ac48960
alexis.guillon <alexis.guillon44@orange.fr>
2024-04-09 08:41:09 +0200

    node4 completed :tada:

---
+++ server.js
@@ -0,0 +1,8 @@
+require('dotenv').config()
+
+function test() {
+    console.log(`I am ${process.env.MY_NAME}, wilder in ${process.env.MY_CITY}, and I love ${process.env.MY_LANGUAGE}`);
+
+}
+
+test()
